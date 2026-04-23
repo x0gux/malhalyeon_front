6bf033e3ad174e2f85109935dca82da7f40d65d3
김태현 <143389711+taehyun00@users.noreply.github.com>
2026-04-23 10:07:08 +0900

    refact : metadata

--- src/_lib/firebase.ts
+++ src/_lib/firebase.ts
@@ -12,7 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase only once
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
